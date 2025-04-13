af78a921ef440ba3ad5d566c418e6b1b6ab42c5e
swhume <swhume@gmail.com>
2025-04-13 13:40:17 -0400

    updates version in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='ndjsonlib',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ndjsonlib'],
     url='https://github.com/swhume/ndjsonlib',
     license='MIT',
