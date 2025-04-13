8b1aa5cc45471ccb7b9dcf8c03cb753640253275
swhume <swhume@gmail.com>
2025-04-13 09:55:26 -0400

    updates setup to have only ndjsonlib package

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='ndjsonlib',
     version='0.0.1',
-    packages=['ndjsonlib', 'ndjsonlib.metadata_file', 'ndjsonlib.json_data_file',
-              'ndjsonlib.dataset_name'],
+    packages=['ndjsonlib'],
     url='https://github.com/swhume/ndjsonlib',
     license='MIT',
     author='Sam Hume',
